e6ca1ac21e66b1ee115695e8f31506a83e0c0425
mingyu-kim0510 <mgmg0510@cu.ac.kr>
2023-11-02 09:31:18 +0900

    feat : 파일 삭제(에러 발생)

--- src/services/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.userService = require('./user.service');
-module.exports.authService = require('./auth.service');
-module.exports.tokenService = require('./token.service');
-module.exports.fileService = require('./file.service');
-module.exports.todoService = require('./todo.service');
-module.exports.boardService = require('./board.service');
--- src/utils/nextId.js
+++
@@ -1,9 +0,0 @@
-const PK = 'id';
-/**
- *
- */
-const nextId = (target) => {
-  const last = target.last().value();
-  return last ? last[PK] + 1 : 1;
-};
-module.exports = nextId;

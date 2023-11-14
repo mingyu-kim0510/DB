d9f822a0561dae7c91bb4c9540734e0e9faa4a3e
Seokjun-Kang <49142108+Seokjun-Kang@users.noreply.github.com>
2023-11-14 14:26:36 +0900

    feat. database 테이블 추가

---
+++ map_test_like_table.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: map_test
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `like_table`
+--
+
+DROP TABLE IF EXISTS `like_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `like_table` (
+  `like_idx` bigint NOT NULL AUTO_INCREMENT,
+  `store_idx` bigint DEFAULT NULL,
+  `user_idx` bigint DEFAULT NULL,
+  PRIMARY KEY (`like_idx`),
+  KEY `FKawoossr0wr2xvkpg1j14g045v` (`store_idx`),
+  KEY `FKky5nonqn1l7n04jkvvu2x6ve5` (`user_idx`),
+  CONSTRAINT `FKawoossr0wr2xvkpg1j14g045v` FOREIGN KEY (`store_idx`) REFERENCES `store_table` (`store_idx`),
+  CONSTRAINT `FKky5nonqn1l7n04jkvvu2x6ve5` FOREIGN KEY (`user_idx`) REFERENCES `user_table` (`user_idx`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `like_table`
+--
+
+LOCK TABLES `like_table` WRITE;
+/*!40000 ALTER TABLE `like_table` DISABLE KEYS */;
+INSERT INTO `like_table` VALUES (32,5614,1);
+/*!40000 ALTER TABLE `like_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-13 10:32:59
